1a8dcaf5e75edf0f0b9cac4abf51d9476f5f598a
unknown <396016240@qq.com>
2019-07-16 10:38:23 +0800

    a

--- src/js/10.js
+++ src/js/10.js
@@ -5,81 +5,114 @@
      * @param {string} p
      * @return {boolean}
      */
-    //不能倒序遍历，根据贪婪搜索的原则，.*会判断错误，并且题目隐含要求实现非贪婪模式
+    //不能倒序遍历，根据贪婪搜索的原则，.*会判断错误
     var isMatch = function(s, p) {
         const COUNT_MATCH="*";
         const CHAR_MATCH=".";
-        let pIndex=0,sIndex=0;
-        let matcher,result=true;
-        //具体的匹配方法
-        function match(i,m,isCount){
-            let result=true;
-            if(isCount){
-                //*匹配
-                let char=m[0];
-                let j=i;
-                if(char===CHAR_MATCH){
-                    i=s.length;
-                }else{
-                    if(s[i]===char){
-                        while(s[i]===char){
-                            i++
-                        }
-                    }
-                }
-                if(i!==j && i-j<m.length-1){
-                    result=false;
-                }
-            }else{
-                //单字符匹配
-                if(m===CHAR_MATCH){
+        let result;
+        /*
+         * 字符串匹配方法
+         * @params {int} i 字符串的起始索引
+         * @params {string} m 正则字符串，单个字符
+         */
+        function match(i,m){
+            let isMatch=true;
+            /*for(let j=0;j<m.length;j++){
+                if(s[i]===m[j] || m[j]===CHAR_MATCH){
                     i++;
+                    continue;
                 }else{
-                    if(s[i++]!==m){
-                        result=false;
-                    }
+                    isMatch=false;
+                    break;
                 }
+            }*/
+            if(s[i]===m || m===CHAR_MATCH){
+                i++;
+            }else{
+                isMatch=false;
             }
             return {
-                result,
-                sIndex:i
+                isMatch,
+                si:i
             }
         }
 
-        //循环主体
-        while(pIndex<p.length && result){
-            //获取matcher
-            switch(p[pIndex+1]){
-                //存在*，题目虽然没有描述，但是测试用例里要求非贪婪模式
-                case COUNT_MATCH:
-                    matcher=p[pIndex]+p[pIndex+1];
-                    pIndex+=2;
-                    //非贪婪模式
-                    while(p[pIndex]===matcher[0]){
-                        matcher+=p[pIndex++];
-                    }
-                    ({result,sIndex}=match(sIndex,matcher,true));
-                    break;
-                default:
-                    matcher=p[pIndex++];
-                    ({result,sIndex}=match(sIndex,matcher));
-                    break;
+        /*
+         * 递归寻找模式
+         * @params {int} si 字符串的起始索引
+         * @params {int} pi 正则的起始索引
+         */
+        function recursive(si,pi){
+            let matcher,isMatch=true;
+            while(pi<p.length && isMatch){
+                //获取matcher
+                switch(p[pi+1]){
+                    //遍历*
+                    case COUNT_MATCH:
+                        matcher=p[pi];
+                        pi+=2;
+                        //计算上限
+                        let max=si;
+                        while((s[max]===matcher || matcher===CHAR_MATCH) && max<s.length){
+                            max++;
+                        }
+                        max-=si;
+                        while(max>=0){
+                            let sii=si+max;
+                            let end=recursive(sii,pi);
+                            if(end){
+                                return true;
+                            }
+                            max--;
+                        }
+                        break;
+                    default:
+                        matcher=p[pi++];
+                        ({isMatch,si}=match(si,matcher));
+                        break;
+                }
+            }
+            if(pi===p.length && si===s.length){
+                return true;
+            }else{
+                return false;
             }
         }
-        //判断s是否被全部消耗
-        if(sIndex<s.length){
-            result=false;
-        }
+        result=recursive(0,0);
         return result;
     };
 
-    console.log(isMatch("aa","a"));
-    console.log(isMatch("aaa","a*"));
-    console.log(isMatch("ab",".*"));
-    console.log(isMatch("aab","c*a*b"));
-    console.log(isMatch("mississippi","mis*is*p*."));
-    console.log(isMatch("mississippi","mis*is*ip*."));
-    console.log(isMatch("aaa","a*aa"));
-    console.log(isMatch("aaa","a*aaa"));
-    console.log(isMatch("aaa","ab*a*c*a"));
+
+    var isMatch1 = function(s, p) {
+        var memo = new Map();
+        var dp = function(i, j) {
+            if(memo.has(i+""+j)) return memo.get(i+""+j);
+            if(j === p.length) return i === s.length;
+            var first = i < s.length && (p.charAt(j) === s.charAt(i) || p.charAt(j) === '.');
+            if(j<=p.length-2 && p.charAt(j+1)==='*') {
+                var ans = dp(i, j+2) || (first && dp(i+1,j));
+            }
+            else ans = first && dp(i+1, j+1);
+            memo.set(i+""+j, ans);
+
+            return ans;
+        }
+        return dp(0, 0);
+    };
+
+    //172ms,34.4MB
+    var isMatch2 = function(s, p) {
+        let pattern=new RegExp("^"+p+"$");
+        return pattern.test(s);
+    };
+
+    //console.log(isMatch("aa","a"));
+    //console.log(isMatch("aaa","a*"));
+    //console.log(isMatch("ab",".*"));
+    //console.log(isMatch("aab","c*a*b"));
+    //console.log(isMatch("mississippi","mis*is*p*."));
+    //console.log(isMatch("mississippi","mis*is*ip*."));
+    //console.log(isMatch("aaa","a*aa"));
+    //console.log(isMatch("aaa","a*aaa"));
+    //console.log(isMatch("aaa","ab*a*c*a"));
 })();
